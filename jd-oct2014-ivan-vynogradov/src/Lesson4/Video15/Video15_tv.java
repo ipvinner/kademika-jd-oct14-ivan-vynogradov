d7bf4356cbd9c442e1e20b5df6f133490e1c9e86
Ivan Vynogradov <vaniavinogradov@rambler.ru>
2014-10-21 14:09:12 +0300

    f

---
+++ jd-oct2014-ivan-vynogradov/src/Lesson4/Video15/Video15_tv.java
@@ -0,0 +1,36 @@
+package Lesson4.Video15;
+
+public class Video15_tv {
+
+	public static void main(String[] args) {
+		
+		double frequency;
+		int diagonal;
+		String model;
+		boolean is3D;
+		double ethernetSpeed;
+		
+
+	}
+	
+	void connectToWiFi(){
+		
+	}
+	
+	void turnOn(){
+		
+	}
+	
+	void turnOff(){
+		
+	}
+	
+	void connectToTVBOX(){
+		
+	}
+	
+	void ejectHDD(){
+		
+	}
+
+}
---
+++ jd-oct2014-ivan-vynogradov/src/Lesson4/Video15/Video15_window.java
@@ -0,0 +1,35 @@
+package Lesson4.Video15;
+
+public class Video15_window {
+
+	public static void main(String[] args) {
+		
+		double width;
+		double height;
+		boolean isMetalloplastikovyj;
+		boolean isGrid;
+		String color;
+		String vendorCountry;
+
+	}
+	
+	void Open(){
+		
+	}
+	
+	void Close(){
+		
+	}
+	
+	void Clean(){
+		
+	}
+	
+	void showColor(){
+		
+	}
+	
+	void show(){
+		
+	}
+}
